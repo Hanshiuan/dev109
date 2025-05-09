6c33f86e382c0bea23056a5de64b1cfb43071cb5
Hanshiuan <hanshiuan.che@bellevuecollege.edu>
2025-05-09 14:56:29 -0700

    Update m5.js

--- m05/m5.js
+++ m05/m5.js
@@ -1,1 +1,13 @@
+var table = prompt("please rnter a nimber from 0 to 10:"); // Ask the user to a number 0-10
 
+
+table=parseInt(table); // convert the input to an integer
+
+if (isNaN(table)|| table===null) { table=0;} //if the input is null,set it to 0
+
+var msg = "<h2>Multiplication Table</h2>"; // a heading for the table
+
+for (var i=1; i,= 10; i++) { msg += i + "X" + table+ "="+ (i*table)+"<br/>";}  // Use a loop to create the table
+
+var el= document.getElementById("blackboard"); 
+el.innerHTML = msg;  // Display the result inside the HTML element with id="blackboard"
